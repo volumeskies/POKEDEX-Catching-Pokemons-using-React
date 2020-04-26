51f7e460979c10e9a9ae3d28f9348e86c1a5abb7
Tatyana Krivorotova <43401852+volumeskies@users.noreply.github.com>
2020-04-26 19:44:15 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import Catched from './Catched'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import RoutePaths from './Constants'
 
-class App extends React.Component{npm 
+class App extends React.Component{
     render(){
         return(
             <div className="App">
